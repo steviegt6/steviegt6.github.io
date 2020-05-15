440f6c73e0e6c8c08319d343859f69cccf19ff3b
Stevie <xxlennygamerxx@gmail.com>
2020-05-15 16:27:36 -0700

    eeeeeeeeeee

--- js/deletefadein.js
+++ js/deletefadein.js
@@ -1,5 +1,4 @@
-$(document).ready(function(){
-    setTimeout(function(){
-       $(".fadeBody").show();
-    }, 8500);
- });
+function deleteFadeIn() {
+    document.getElementsByClassName("fadeBody").style[0] = "opacity: 1;"
+}
+document.onload = setTimeout(deleteFadeIn, 8500);
