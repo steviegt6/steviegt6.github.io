d5697a5b677ff958e96d0efd29ba7190a906f714
Stevie <xxlennygamerxx@gmail.com>
2021-03-22 22:24:26 -0700

    stuff

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
 var error404URLs = [
     "https://http.cat/404",
-    "https://httpstatusdogs.com/img/404.jpg"
+    "https://httpstatusdogs.com/img/404.jpg",
+    "https://i.pinimg.com/originals/9e/89/e9/9e89e9298bea2893ea034296a897ff70.jpg",
+    "https://i.chzbgr.com/full/8424964096/h76440262/address-unknown"
 ];
 
 function getRandomImage(name, id) {
-    var array = name == "error404URLs" ? error404URLs : [""];
-
-    if (array == [""]) {
-        throw "Array was empty, input was: " + name;
-    }
-
+    var array = processImageArrayInput(name);
     document.getElementById(id).innerHTML = "<img src =\"" + array[Math.floor(Math.random() * array.length)] + "\"/>";
+}
+
+function processImageArrayInput(name) {
+    name = name.toLowerCase();
+
+    switch (name) {
+        case "error404urls":
+            return error404URLs;
+        
+        default:
+            throw "Array not found, input was: " + name;
+    }
 }
