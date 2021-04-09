1614491b81f55700c5d06ae9596844f1f9d94c20
Glitch (maroon-incandescent-layer) <none>
2021-04-09 17:21:05 +0000

    familiarizing myself more with glitch + js prep

--- js/main.js
+++ js/main.js
@@ -1,30 +1,32 @@
 // all functions & other important stuff below
-window.addEventListener("load", loadEvent, false)
+window.addEventListener("load", loadEvent, false);
+
+// main load event, nothing all that important
+function loadEvent() {}
+
+function handleCustomLinkLogic(url) {}
 
 var error404URLs = [
-    "https://http.cat/404",
-    "https://httpstatusdogs.com/img/404.jpg",
-    "https://i.pinimg.com/originals/9e/89/e9/9e89e9298bea2893ea034296a897ff70.jpg",
-    "https://i.chzbgr.com/full/8424964096/h76440262/address-unknown"
+  "https://http.cat/404",
+  "https://httpstatusdogs.com/img/404.jpg",
+  "https://i.pinimg.com/originals/9e/89/e9/9e89e9298bea2893ea034296a897ff70.jpg",
+  "https://i.chzbgr.com/full/8424964096/h76440262/address-unknown"
 ];
 
 function getRandomImage(name, id) {
-    var array = processImageArrayInput(name);
-    document.getElementById(id).innerHTML = "<img src =\"" + array[Math.floor(Math.random() * array.length)] + "\"/>";
+  var array = processImageArrayInput(name);
+  document.getElementById(id).innerHTML =
+    '<img src ="' + array[Math.floor(Math.random() * array.length)] + '"/>';
 }
 
 function processImageArrayInput(name) {
-    name = name.toLowerCase();
+  name = name.toLowerCase();
 
-    switch (name) {
-        case "error404urls":
-            return error404URLs;
-        
-        default:
-            throw "Array not found, input was: " + name;
-    }
-}
+  switch (name) {
+    case "error404urls":
+      return error404URLs;
 
-function handleCustomLinkLogic(url) {
-  
+    default:
+      throw "Array not found, input was: " + name;
+  }
 }
