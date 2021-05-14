05a26724bfc4da198072819b3c7adf069cfb521a
Glitch (maroon-incandescent-layer) <none>
2021-05-14 16:53:02 +0000

    ⛱🍔 Checkpoint ./css/main.css:3644664/492 ./assets/particles.json:3644664/112 ./js/particleHandler.js:3644664/826

--- js/particleHandler.js
+++ js/particleHandler.js
@@ -1,26 +1,26 @@
-particlesJS("particles-js", {
+window.particlesJS("particles-js", {
   particles: {
-    number: { value: 80, density: { enable: true, value_area: 800 } },
+    number: { value: 150, density: { enable: true, value_area: 800 } },
     color: { value: "#000000" },
-    shape: {
+    /*shape: {
       type: "circle",
       stroke: { width: 0, color: "#000000" },
       polygon: { nb_sides: 5 },
       image: { src: "img/github.svg", width: 100, height: 100 }
-    },
+    },*/
     opacity: {
       value: 0.5,
-      random: false,
+      random: true,
       anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
     },
     size: {
-      value: 3,
+      value: 6,
       random: true,
       anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
     },
     line_linked: {
       enable: true,
-      distance: 150,
+      distance: 200,
       color: "#000000",
       opacity: 0.4,
       width: 1
@@ -39,8 +39,8 @@ particlesJS("particles-js", {
   interactivity: {
     detect_on: "canvas",
     events: {
-      onhover: { enable: true, mode: "repulse" },
-      onclick: { enable: true, mode: "push" },
+      onhover: { enable: false, mode: "repulse" },
+      onclick: { enable: false, mode: "push" },
       resize: true
     },
     modes: {
@@ -53,20 +53,3 @@ particlesJS("particles-js", {
   },
   retina_detect: true
 });
-var count_particles, stats, update;
-stats = new Stats();
-stats.setMode(0);
-stats.domElement.style.position = "absolute";
-stats.domElement.style.left = "0px";
-stats.domElement.style.top = "0px";
-document.body.appendChild(stats.domElement);
-count_particles = document.querySelector(".js-count-particles");
-update = function() {
-  stats.begin();
-  stats.end();
-  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-  }
-  requestAnimationFrame(update);
-};
-requestAnimationFrame(update);
